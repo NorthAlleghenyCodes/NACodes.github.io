5d98316e8b72423506178a5030250dc933b87f91
NorthAlleghenyCodes <36904724+NorthAlleghenyCodes@users.noreply.github.com>
2018-02-27 19:16:19 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    var controller = new ScrollMagic.Controller();
+    var tween = TweenMax.to(".headimg",3,{opacity:1,y:0,delay:.75,ease:Power2.easeOut});
+    var ourScene = new ScrollMagic.Scene({
+        triggerElement:".title",
+        triggerHook:0.6,
+        reverse:false
+        
+        
+    })
+    .setTween(tween)
+    
+    .addTo(controller);
+    var tween2 = TweenMax.to(".top-button",3,{opacity:1,y:-35,delay:1.75,ease:Power2.easeOut});
+    var ourScene2 = new ScrollMagic.Scene({
+        triggerElement:".headimg",
+        triggerHook:0.6,
+        reverse:false
+        
+        
+    })
+    .setTween(tween2)
+    .addTo(controller);
+});
