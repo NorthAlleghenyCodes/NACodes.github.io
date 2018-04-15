3135378f40e507d33a54ce15812a16c35b04f608
OptimusIacobus <supajake118@gmail.com>
2018-04-15 01:26:32 -0400

    Added animation toggle with scrollMagic

--- index.js
+++ index.js
@@ -5,20 +5,28 @@ $(document).ready(function(){
         triggerElement:".title",
         triggerHook:0.6,
         reverse:false
-        
-        
+
+
     })
     .setTween(tween)
-    
+
     .addTo(controller);
     var tween2 = TweenMax.to(".top-button",3,{opacity:1,y:-35,delay:1.75,ease:Power2.easeOut});
     var ourScene2 = new ScrollMagic.Scene({
         triggerElement:".headimg",
         triggerHook:0.6,
         reverse:false
-        
-        
+
+
     })
     .setTween(tween2)
+    .addTo(controller);
+
+    var scene = new ScrollMagic.Scene({
+        triggerElement: ".section",
+        triggerHook: 0.95
+    })
+
+    .setClassToggle('.section', 'bounceInDown')
     .addTo(controller);
 });
