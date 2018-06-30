1a975da521e782688a09bd03f30ddfe7767abea3
NorthAlleghenyCodes <36904724+NorthAlleghenyCodes@users.noreply.github.com>
2018-06-30 13:49:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    initCountdown();
     var controller = new ScrollMagic.Controller();
     var tween = TweenMax.to(".headimg",3,{opacity:1,y:0,delay:.75,ease:Power2.easeOut});
     var ourScene = new ScrollMagic.Scene({
@@ -11,7 +12,7 @@ $(document).ready(function(){
     .setTween(tween)
     
     .addTo(controller);
-    var tween2 = TweenMax.to(".top-button",3,{opacity:1,y:-35,delay:1.75,ease:Power2.easeOut});
+    var tween2 = TweenMax.to(".countdown-width",3,{opacity:1,y:-35,delay:1.75,ease:Power2.easeOut});
     var ourScene2 = new ScrollMagic.Scene({
         triggerElement:".headimg",
         triggerHook:0.6,
@@ -20,6 +21,16 @@ $(document).ready(function(){
         
     })
     .setTween(tween2)
+    .addTo(controller);
+     var tween3 = TweenMax.to(".clocktext",3,{opacity:1,y:-15,delay:2.75,ease:Power2.easeOut});
+    var ourScene3 = new ScrollMagic.Scene({
+        triggerElement:".headimg",
+        triggerHook:0.6,
+        reverse:false
+        
+        
+    })
+    .setTween(tween3)
     .addTo(controller);
     
      $("a").on('click', function(event) {
@@ -60,6 +71,25 @@ for (i = 0; i < coll.length; i++) {
     } 
   });
 }
+
+ function initCountdown(){
+        var deadline = "2019-04-26T03:14:15.927-05:00" //April 16, 2019 at 3:14:15.927 am est
+
+        //update clock every second
+        setInterval(function(){
+            var millis = Date.parse(deadline) - Date.now();
+            var seconds = Math.floor((millis / 1000) % 60);
+            var min = Math.floor((millis / 1000 / 60)% 60);
+            var hours = Math.floor((millis / 1000 / 60 / 60) % 24);
+            var days = Math.floor((millis / 1000 / 60 / 60 / 24));
+
+            document.getElementById("countdown-days").innerHTML = (days < 10 ? "0" + days : days);
+            document.getElementById("countdown-hours").innerHTML = (hours < 10 ? "0" + hours : hours);
+            document.getElementById("countdown-minutes").innerHTML = (min < 10 ? "0" + min : min);
+            document.getElementById("countdown-seconds").innerHTML = (seconds < 10 ? "0" + seconds : seconds);
+        }, 500);
+
+    }
 
 
 });
